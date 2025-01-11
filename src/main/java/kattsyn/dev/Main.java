e03deaa0f9f7ba69e41ad6e9c64f70d19e11fd1e
Kattsyn <vov4ikkat3@mail.ru>
2025-01-12 01:41:09 +0300

    added some test code

--- src/main/java/kattsyn/dev/Main.java
+++ src/main/java/kattsyn/dev/Main.java
@@ -6,6 +6,12 @@ import java.util.Map;
 
 public class Main {
 
+    public static final String FLOAT_REGEX = "/^-?(0|[1-9]+)(?:[.]\\d{1,2}|)$/";
+    public static final String INTEGER_REGEX = "[0-9]+";
+    public static final String STRING_REGEX = ".*";
+
+    public static final String[] regexes = new String[]{FLOAT_REGEX, INTEGER_REGEX, STRING_REGEX};
+
     public static final String TEST_INPUT_1 = """
             agasga
             skwk
@@ -24,6 +30,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         System.out.println("Вывод первого файла:\n " + TEST_INPUT_1);
         System.out.println();
         System.out.println("Вывод второго файла:\n " + TEST_INPUT_2);
@@ -39,9 +46,33 @@ public class Main {
 
         System.out.println(stringList);
 
-        for (String string : stringList) {
+        String[] classesNames = new String[]{"Float, Integer, String"};
 
+        List<Float> floats = new ArrayList<>();
+        List<Integer> integers = new ArrayList<>();
+        List<String> strings = new ArrayList<>();
+
+
+        for (String string : stringList) {
+            if (string.matches(FLOAT_REGEX)) {
+                floats.add(Float.parseFloat(string));
+            } else if (string.matches(INTEGER_REGEX)) {
+                integers.add(Integer.parseInt(string));
+            } else {
+                strings.add(string);
+            }
         }
+
+        System.out.println("floats: " + floats);
+        System.out.println("integers: " + integers);
+        System.out.println("strings: " + strings);
+
+        //Сбор статистики
+        //short stats
+        System.out.println("Всего floats: " + floats.size());
+        System.out.println("Всего integers: " + integers.size());
+        System.out.println("Всего strings: " + strings.size());
+
 
     }
 
