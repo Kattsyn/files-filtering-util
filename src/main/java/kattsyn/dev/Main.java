187f8362ab7feb595ab5b62239141b0d73b7ed1c
Kattsyn Vladimir Yurievich <78604784+Kattsyn@users.noreply.github.com>
2025-01-13 23:54:30 +0300

    added File creating and writing test code

--- src/main/java/kattsyn/dev/Main.java
+++ src/main/java/kattsyn/dev/Main.java
@@ -1,5 +1,8 @@
 package kattsyn.dev;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -63,6 +66,13 @@ public class Main {
 
         //todo: мапу, где ключ - имя типа данных, а значение ArrayList
 
+
+        //todo: создание файлов делать так:
+        /*
+        перебирать вхождения в HashMap. Если entry.value.size > 0, то создаем
+        файл по имени (доп строка в параметрах)+entry.key
+         */
+
         for (String string : stringList) {
             if (string.matches(FLOAT_REGEX)) {
                 floats.add(Float.parseFloat(string));
@@ -79,9 +89,11 @@ public class Main {
 
         //Сбор статистики
         //short stats
+        System.out.println("SHORT STATS");
         System.out.println("Всего вещественных чисел записано: " + floats.size());
         System.out.println("Всего целых чисел записано: " + integers.size());
         System.out.println("Всего строк записано: " + strings.size());
+        System.out.println();
 
         //full stats
         float min = Float.MAX_VALUE;
@@ -96,14 +108,43 @@ public class Main {
                 sum += value;
             }
         }
+        /*
+        Без параметра записи в текущие файлы нужно будет создать файлы, проверить
+        существует ли такой файл, если да, то перезаписать информацию и возможно вывести об этом сообщение
+        если нет, то просто записать данные в файл. Метод записи в файл должен иметь на вход имя создания файла,
+        булевскую переменную ПерезаписатьЛи, путь по которому создать файл и ArrayList строк которые нужно записать в файл.
+        Можно добавить обработку является ли передаваемый путь директорией.
+         */
 
+        System.out.println("FULL STATS");
         System.out.println("min: " + min);
         System.out.println("max: " + max);
         System.out.println("sum: " + sum);
         System.out.println("avg: " + sum / (floats.size() + integers.size()));
 
+        System.out.println();
 
+
+
+        String FLOATS_FILE_NAME = "floats.txt";
+        //todo: вынести в отдельный метод создания файла, если нет режима записи в имеющийся
+        try {
+            File floatFile = new File(FLOATS_FILE_NAME);
+            if (floatFile.createNewFile()) {
+                System.out.println("File created: " + floatFile.getAbsolutePath());
+            } else {
+                System.out.println("File already exists: " + floatFile.getAbsolutePath());
+            }
+        } catch (Exception e) {
+            System.out.println("Unexpected exception while file creating: " + e.getMessage());
+        }
+
+        try (FileWriter floatsWriter = new FileWriter(FLOATS_FILE_NAME)) {
+            for (Float f : floats) {
+                floatsWriter.write(f + "\n");
+            }
+        } catch (IOException exception) {
+            System.out.println("Unexpected exception while writing file: " + exception.getMessage());
+        }
     }
-
-
 }
